a904f8789783ad9126ec61bd4e9ac2f7728e6077
Edward Purwanto <edward.purwanto@students.mq.edu.au>
2019-09-25 15:43:58 +1000

    Bootstrap imports, Sass and basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      <main className="content">
+        <h1 className="display-1 text-center">gANYrator</h1>
+        <h2 className="text-center">Generate Anything.</h2>
+      </main>
     </div>
   );
 }
