96862a569644e23baa43e4efdbdb9ae42e34a0c7
Edward Purwanto <edward.purwanto@students.mq.edu.au>
2020-06-19 13:48:11 +1000

    Moved intro to the top of roll page

--- src/roll/ScreenRoll.js
+++ src/roll/ScreenRoll.js
@@ -90,6 +90,18 @@ class ScreenRoll extends Component {
             <React.Fragment>
 
                 <Row>
+                    <Col sm="12">
+                        <h3>What's a gANYrator?</h3>
+                    </Col>
+                    <Col sm="12">
+                        <p>gANYrator is a content generation system for tabletop RPG's such as Dungeons and Dragons. Build
+                            tables of content, link them together with actions, and then use them to roll random NPC's,
+                            Towns, Loot or more. You can get started by creating your own tables or playing around with
+                            the default tables.</p>
+                    </Col>
+                </Row>
+
+                <Row>
                     {/* Last rolled field Values */}
                     {
                         Object.entries(this.state.values).map(([key, val]) => {
@@ -121,15 +133,6 @@ class ScreenRoll extends Component {
                     </Col>
                 </Row>
 
-                <Row>
-                    <Col sm="12">
-                        <h3>What's a gANYrator?</h3>
-                    </Col>
-                    <Col sm="12">
-                        <p>gANYrator is a content generation system for tabletop RPG's such as Dungeons and Dragons.</p>
-                        <p>Build tables of content, link them together with actions, and then use them to roll random NPC's, Towns, Loot or more.</p>
-                    </Col>
-                </Row>
             </React.Fragment>
         );
     }
